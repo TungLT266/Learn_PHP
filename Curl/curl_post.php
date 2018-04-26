4933806d3546889960c5da8846db4ab1d1e79af6
TungLT266 <37206096+TungLT266@users.noreply.github.com>
2018-04-26 22:57:45 +0700

    update curl_post

--- Curl/curl_post.php
+++ Curl/curl_post.php
@@ -12,9 +12,11 @@ function curl($url)
 function curlPost($url, $param)
 {
     $ch = curl_init($url);
+
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
     $output = curl_exec($ch);
     curl_close($ch);
     return $output;
@@ -23,12 +25,13 @@ function curlPost($url, $param)
 $url = 'https://www.thegioididong.com/dtdd/oppo-f7-128gb'; // link 1 sản phẩm của thế giới di động
 $content = curl($url);
 
-if(preg_match('~var GL_PRODUCTID = (\d+)~', $content, $productIDMatch)){
-    if(preg_match_all('~onclick="gotoGallery\(([17]),(\d+)\)"~', $content, $imageTypeAndColorIDMatch)) {
+if (preg_match('~var GL_PRODUCTID = (\d+)~', $content, $productIDMatch)) {
+    if (preg_match_all('~onclick="gotoGallery\(([17]),(\d+)\)"~', $content, $imageTypeAndColorIDMatch)) {
         $imageList = array();
         for ($i = 0; $i < sizeof($imageTypeAndColorIDMatch[0]); $i++) {
             $param = 'productID=' . $productIDMatch[1] . '&imageType=' . $imageTypeAndColorIDMatch[1][$i] . '&colorID=' . $imageTypeAndColorIDMatch[2][$i];
             $content = curlPost('https://www.thegioididong.com/aj/ProductV4/GallerySlideFT/', $param);
+
             if (preg_match_all('~data-img="(.+?)"~', $content, $imageMatch)) {
                 if ((int)$imageTypeAndColorIDMatch[1][$i] == 1) {
                     foreach ($imageMatch[1] as $item) {
@@ -39,7 +42,6 @@ if(preg_match('~var GL_PRODUCTID = (\d+)~', $content, $productIDMatch)){
                         $imageList[] = $item;
                     }
                 }
-
             }
         }
         var_dump($imageList);
